751abfebf487034912a502f48d021952c3c40d92
Furufuru <huruhuru1212@gmail.com>
2013-10-29 01:05:08 +0900

    commit.

--- furufuru1.1c/single.php
+++ furufuru1.1c/single.php
@@ -1,25 +1,22 @@
-<? get_header(); ?>
+<?get_header(); ?>
 	<div style="padding-top: 20px;" />
 	<div id="main">
 		<div class="maincontent">
-		<? if (have_posts()) : ?>
-                                        <? while (have_posts()) : the_post(); ?>
+		<?if (have_posts()) : ?>
+                                        <?while (have_posts()) : the_post(); ?>
 		<div class="post">
-		<div class="blog_info">
-			<ul>
-			<li class="cal"><? the_time('Y/m/d') ?></li>
-			</ul>
-			<br style="clear: both;">
+		<div class="post_info">
+			<? the_time('Y/m/d') ?>
 		</div>
 		<h2 class="title"><? the_title(); ?></h2>
-		<? the_content(); ?>
+		<?the_content(); ?>
 		<div style="padding-bottom:20px;"></div>
 		</div>
 		<? endwhile; ?>
-		<? comments_template(); ?>
+		<?comments_template(); ?>
 		</div>
 	</div>
 </div>
-<? endif; ?>
+<?endif; ?>
 
-<? get_footer(); ?>
+<?get_footer(); ?>
