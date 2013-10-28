c61434e0c3eb3b53345d00c782f652f4b735b72c
Furufuru <huruhuru1212@gmail.com>
2013-10-29 01:10:47 +0900

    commit

--- furufuru1.1c/single.php
+++ furufuru1.1c/single.php
@@ -1,20 +1,20 @@
 <?get_header(); ?>
 	<div style="padding-top: 20px;" />
 	<div id="main">
-		<div class="maincontent">
-		<?if (have_posts()) : ?>
-                                        <?while (have_posts()) : the_post(); ?>
-		<div class="post">
-		<div class="post_info">
-			<? the_time('Y/m/d') ?>
-		</div>
-		<h2 class="title"><? the_title(); ?></h2>
-		<?the_content(); ?>
-		<div style="padding-bottom:20px;"></div>
-		</div>
-		<? endwhile; ?>
-		<?comments_template(); ?>
-		</div>
+			<div class="maincontent">
+				<?if (have_posts()) : ?>
+                    		                    <?while (have_posts()) : the_post(); ?>
+				<div class="post">
+				<div class="post_info">
+					<? the_time('Y/m/d') ?>
+				</div>
+				<h2 class="title"><? the_title(); ?></h2>
+				<?the_content(); ?>
+				<div style="padding-bottom:20px;"></div>
+				</div>
+				<? endwhile; ?>
+				<?comments_template(); ?>
+			</div>
 	</div>
 </div>
 <?endif; ?>
